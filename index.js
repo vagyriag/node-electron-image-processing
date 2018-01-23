876644f10128bdc919687a9f8a316d3c282a127c
Sebastian Gaviria Giraldo <sebastiangaviria@outlook.com>
2018-01-23 11:43:43 -0500

    configure sizes and ignore smaller files

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const fs = require('fs'),
       mkdirp = require('mkdirp');
 
 const optionDefinitions = [
- { name: 'origin', alias: 'o', type: String },
- { name: 'destination', alias: 'd', type: String, defaultValue: 'result' },
- { name: 'thumb', alias: 't', type: Boolean, defaultValue: false },
- { name: 'rename', alias: 'r', type: Boolean, defaultValue: false }
+  { name: 'origin',        alias: 'o', type: String                          },
+  { name: 'destination',   alias: 'd', type: String, defaultValue: 'result'  },
+  { name: 'thumb',         alias: 't', type: Boolean, defaultValue: false    },
+  { name: 'rename',        alias: 'r', type: Boolean, defaultValue: false    },
+  { name: 'ignoreSmaller', alias: 'i', type: Boolean, defaultValue: false    },
+  { name: 'width',         alias: 'W', type: Number, defaultValue: 800       },
+  { name: 'height',        alias: 'H', type: Number, defaultValue: undefined },
+  { name: 'thumbWidth',    alias: 'w', type: Number, defaultValue: 220       },
+  { name: 'thumbHeight',   alias: 'h', type: Number, defaultValue: 150       },
 ];
 const options = commandLineArgs(optionDefinitions);
-const { origin, destination, thumb, rename } = options;
+const { origin, destination, thumb, rename, ignoreSmaller, width, height, thumbWidth, thumbHeight } = options;
 
 if(!origin) return console.log('--origin parameter is mandatory');
 
@@ -48,18 +53,25 @@ list.forEach((orig, i) => {
   if(lastFolder != folder) (lastFolder = folder) && (lastIndex = i);
   if(rename) name = i + 1 - lastIndex;
 
-  var img = sharp(orig)
-    .resize(800)
-    .jpeg({ progressive: true });
-  
-  img.clone().toFile(`${folder}/${name}.jpg`, (err) =>
-    console.log(`progress: ${++count} of ${list.length}`));
+  var img = sharp(orig);
 
-  if(!thumb) return;
-  img.resize(220, 150)
-    .crop(sharp.strategy.entropy)
-    .jpeg({ quality:60, progressive: true });
-  
-  img.toFile(`${folder}/${name}_thumb.jpg`);
+  img.metadata().then((meta) => {
+    if(ignoreSmaller && meta.width <= width) return console.log(`progress: ${++count} of ${list.length} - ignored`);
+
+    img.resize(width, height)
+      .withoutEnlargement()
+      .crop(sharp.strategy.entropy)
+      .jpeg({ progressive: true });
+    
+    img.clone().toFile(`${folder}/${name}.jpg`, (err) =>
+      console.log(`progress: ${++count} of ${list.length}`));
+
+    if(!thumb) return;
+    img.resize(thumbWidth, thumbHeight)
+      .crop(sharp.strategy.entropy)
+      .jpeg({ quality:60, progressive: true });
+    
+    img.toFile(`${folder}/${name}_thumb.jpg`);
+
+  });
 });
-
